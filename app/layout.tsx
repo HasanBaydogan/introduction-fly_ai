82aea8341f03ea3a779216793c667674c614a501
Hasan Baydoğan <hasan.baydogan@asparel.com.tr>
2025-09-17 10:57:58 +0300

    vercel analytics entegrated

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next';
+import { Analytics } from '@vercel/analytics/next';
 
 import './globals.css'
 import Navbar from '@/components/Navbar';
@@ -49,6 +50,7 @@ export default function RootLayout({
           </main>
           <Footer />
         </LanguageProvider>
+        <Analytics />
       </body>
     </html>
   )
